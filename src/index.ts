04c5396159cd07cc86dc406191afe604f7fe8d5a
Adityesh <adityeshmishra98@gmail.com>
2021-01-06 06:40:36 -0500

    Socket integration

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,61 @@
-import express from 'express';
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+import express, { NextFunction, Request, Response } from 'express';
+import TempModel from './Model/TempModel';
 import mongoose from 'mongoose';
 import Router from './Routes/Routes';
-// const dotenv = require('dotenv').config();
+import http from 'http';
+import cron from 'node-cron';
+
+
+const socket = require('socket.io');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+const io = socket(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+
+const PORT = process.env.PORT || 5000;
 const KEY = process.env.THINK_SPEAK_API_KEY;
 const DB_URI = process.env.DB_URI;
+
 const db = mongoose.connection;
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
 mongoose.connect(DB_URI!, ({useNewUrlParser : true, useUnifiedTopology : true}));
 
 db.once('open', () => {
-    console.log(`Connected to database...`)
+    console.log(`Connected to database...`);
+    
+
+    io.on('connection', (socket : any) => {
+        const job = cron.schedule('*/10 * * * * *', async () => {
+            //Insert a document every 1 minute about temp and pulse
+            const newTemp = new TempModel({
+                bodyPulse : randomNumber(60, 90),
+                bodyTemp : randomNumber(35, 39)
+            });
+    
+            const doc = await newTemp.save();
+            if(doc) {
+                console.log(doc + "\n ---------------");
+                socket.emit('Temp', doc)
+            }
+            
+            
+        });
+
+        socket.on('disconnect', () => {
+            job.destroy();
+        })
+    })
 })
 
 
@@ -21,10 +63,19 @@ app.get('/', (req : express.Request, res : express.Response) => {
     res.status(200).send("<h1>Hello World</h1>");
 })
 
+app.use('/api', Router);
 
 
-app.use('/api', Router)
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+})
+
+
+
+
+
+function randomNumber(min : number, max : number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
