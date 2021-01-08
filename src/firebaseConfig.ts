ccf2487803304c6b5af954ed89155d673b9cc020
harisumaid27 <harisumaid27@gmail.com>
2021-01-08 23:11:49 +0530

    removed files containing sensitive information

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYEgp2ZeFPMcVnkEDvdD7KEWR6rb8DXFU",
+    authDomain: "companion-a27d5.firebaseapp.com",
+    databaseURL: "https://companion-a27d5.firebaseio.com",
+    projectId: "companion-a27d5",
+    storageBucket: "companion-a27d5.appspot.com",
+    messagingSenderId: "26925186540",
+    appId: "1:26925186540:web:328f1071c5dfd1143b54c9"
+  };
+
+export default firebaseConfig;
