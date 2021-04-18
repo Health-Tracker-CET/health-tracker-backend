58128285d0347b263f7506e9a998e48d032a01df
abhishek-ottosero <abhishek@ottosero.com>
2021-04-18 23:49:49 +0530

    Added AttendantController and Attendant schema file

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -3,6 +3,7 @@ import {Router} from 'express';
 import { getBodyData, getAbnormalBodyData } from '../Controller/Controller';
 import  firebaseController  from '../Controller/FirebaseController';
 import { getDoctors } from '../Controller/DoctorController';
+import { addUserToAttendant, createAttendant, getAllUsers, loginAttendant } from '../Controller/AttendantController';
 // Init the router var from express
 const router = Router();
 
@@ -20,6 +21,20 @@ router.post('/abnormal', getAbnormalBodyData);
 
 // Get all the doctors from the doctor collection
 router.get('/doctor/all', getDoctors);
+
+// Attendant Routes
+
+// Creating a new Attendant
+router.post('/create-attendant',createAttendant);
+
+// Logging in the Attendant
+router.post("/login-attendant",loginAttendant);
+
+// Add user to attendant
+router.post("/add-user-to-attendant",addUserToAttendant);
+
+// Get All the Attendant Users
+router.post("/get-all-attendant-users",getAllUsers)
 
 
 // Exports
